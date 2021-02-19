21e360341b388ac5092b003ec4ce710352d726ea
Astel <astelthottankara@gmail.com>
2021-02-19 13:00:44 +0530

    fix: allow cors

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,12 @@ import axios from 'axios';
 
 const URL = 'https://newsapi.org/v2/everything?';
 
+const config = {
+	'Access-Control-Allow-Origin': '*',
+};
+
 export const fetchNews = query =>
-	axios.get(URL + `q=${query}&apiKey=${process.env.REACT_APP_NEWS_API}`);
+	axios.get(
+		URL + `q=${query}&apiKey=${process.env.REACT_APP_NEWS_API}`,
+		config
+	);
